120528ebfc861cacb566bff501389eff4dccd178
zsteve <stephenz@student.unimelb.edu.au>
2021-03-04 18:44:55 -0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="statot", # Replace with your own username
-    version="0.0.12",
+    version="0.0.13",
     author="Stephen Zhang",
     author_email="syz@math.ubc.ca",
     description="statOT",
